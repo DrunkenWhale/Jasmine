a0c55be4ff27f82cb560724d4ed2930044b73f93
pigeon_gugugu <892197240@qq.com>
2022-04-13 00:11:04 +0800

    feat: part of node

--- cache/cache.go
+++ cache/cache.go
@@ -3,12 +3,16 @@ package cache
 import "time"
 
 type Cache struct {
-	core map[string]*Value
+	core      map[string]*Value
+	memory    int
+	maxMemory int
 }
 
-func NewCache() *Cache {
+func NewCache(maxMemory int) *Cache {
 	return &Cache{
-		core: make(map[string]*Value),
+		core:      make(map[string]*Value),
+		memory:    0,
+		maxMemory: maxMemory,
 	}
 }
 
@@ -16,6 +20,7 @@ func (c *Cache) Get(key string) ([]byte, bool) {
 	if res, ok := c.core[key]; ok {
 		if time.Now().Unix() > res.expiration {
 			delete(c.core, key)
+			c.memory -= len(res.value)
 			return nil, false
 		}
 		return res.value, true
@@ -26,4 +31,13 @@ func (c *Cache) Get(key string) ([]byte, bool) {
 
 func (c *Cache) Put(key string, value []byte, expiration int64) {
 	c.core[key] = NewValue(value, expiration)
+	c.memory += len(value)
+}
+
+func (c *Cache) CacheMemory() int {
+	return c.memory
+}
+
+func (c *Cache) MaxCacheMemory() int {
+	return c.maxMemory
 }
--- node/node.go
+++ node/node.go
@@ -5,3 +5,12 @@ import "Jasmine/cache"
 type Node struct {
 	cache *cache.Cache
 }
+
+func (node *Node) Get(key string) ([]byte, bool) {
+	return node.cache.Get(key)
+}
+
+func (node *Node) Put(key string, value []byte, respiration int64) {
+	node.cache.Put(key, value, respiration)
+}
+
