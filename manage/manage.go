7039f8603837787ef26e724661356a1bbc94cd63
pigeon_gugugu <892197240@qq.com>
2022-04-13 10:49:11 +0800

    feat: part of manage

--- manage/manage.go
+++ manage/manage.go
@@ -1,18 +1,35 @@
 package manage
 
+import "Jasmine/consistent"
+
 type Manager struct {
 
 	// key 	 : node name
 	// value : node host
 	nodes map[string]string
+
+	hashMap *consistent.Map
 }
 
 func NewManger() *Manager {
 	return &Manager{
-		nodes: make(map[string]string),
+		nodes:   make(map[string]string),
+		hashMap: consistent.NewMap(nil),
 	}
 }
 
-func Add(nodeName string, nodeAddress string) {
+func (m *Manager) AddNode(nodeName string, nodeAddress string) {
+	m.nodes[nodeName] = nodeAddress
+}
 
+func (m *Manager) Register() {
+	var t []string
+	for k, _ := range m.nodes {
+		t = append(t, k)
+	}
+	m.hashMap.Add(t...)
+}
+
+func (m *Manager) FindNode(key string) string {
+	return m.hashMap.Get(key)
 }
